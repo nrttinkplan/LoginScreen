d0a7316dbcb35a082d283418946a9bd6b5d3273f
nrttinkplan <erhannurettinkplan14@gmail.com>
2024-10-28 17:10:42 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,148 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function LoginScreen() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <LinearGradient
+      colors={['rgba(248, 140, 140, 0.61)', '#000000', '#514E4E', 'rgba(0, 0, 0, 0)']}
+      locations={[0, 0.9998, 0.9999, 1]}
+      style={styles.container}
+    >
+      <View style={styles.imgContainer}>
+        <Image
+          source={require('./assets/background.png')}
+          style={styles.backgroundImg}
+        />
+        <View style={styles.imgTextContainer}>
+          <Text style={styles.sampleText}>SAMPLE</Text>
+          <Text style={styles.iotAppText}>IOT APP</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+      <View style={styles.formContainer}>
+        <Text style={styles.signInText}>Sign In</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Email ID"
+            placeholderTextColor="#fff"
+            style={styles.input}
+          />
+          <Image
+            source={require('./assets/email.png')}
+            style={styles.icon}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Enter Password"
+            placeholderTextColor="#fff"
+            secureTextEntry
+            style={styles.input}
+          />
+          <Image
+            source={require('./assets/lock.png')}
+            style={styles.icon}
+          />
+        </View>
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>Log In</Text>
+        </TouchableOpacity>
+        <Text style={styles.footerText}>Don’t have an account yet?</Text>
+        <TouchableOpacity>
+          <Text style={styles.createAccountText}>Create an account</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  imgContainer: {
+    position: 'relative',
   },
-  sectionDescription: {
-    marginTop: 8,
+  backgroundImg: {
+    width: 414,
+    height: 480,
+    borderWidth: 1,
+    borderColor: '#D04A4A',
+  },
+  imgTextContainer: {
+    position: 'absolute',
+    bottom: 30,
+    left: 40,
+  },
+  sampleText: {
+    fontSize: 30,
+    color: '#000',
+    marginBottom: 1,
+  },
+  iotAppText: {
+    fontSize: 78,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  formContainer: {
+    width: '70%',
+    marginTop: 50,
+  },
+  signInText: {
+    fontSize: 17,
+    fontFamily: 'Roboto',
+    color: '#fff',
+    marginBottom: 10,
+    marginLeft: 1,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: '#FDA43C',
+    borderRadius: 10,
+    marginBottom: 20,
+    paddingHorizontal: 25,
+  },
+  icon: {
+    width: 17,
+    height: 17,
+    left: 0,
+  },
+  input: {
+    flex: 1,
+    height: 50,
+    color: '#fff',
+    fontSize: 17,
+    fontFamily: 'Roboto',
+  },
+  loginButton: {
+    height: 52,
+    backgroundColor: '#FDA43C',
+    borderRadius: 10,
+    paddingVertical: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  loginButtonText: {
+    color: '#000',
     fontSize: 18,
-    fontWeight: '400',
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    marginTop: 3,
   },
-  highlight: {
-    fontWeight: '700',
+  footerText: {
+    color: '#fff',
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  createAccountText: {
+    color: '#FDA43C',
+    textAlign: 'center',
+    marginTop: 5,
+    textDecorationLine: 'underline',
   },
 });
-
-export default App;
